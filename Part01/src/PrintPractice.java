81b27cce62941be26db37d7dcc239cda445b22e3
brandon <86nguyenb@gmail.com>
2016-10-16 01:42:30 -0400

    Initial commit

---
+++ Part01/src/PrintPractice.java
@@ -0,0 +1,31 @@
+/**
+ * PrintPractice.java
+ *
+ * A program that tests console print.
+ *
+ * @author Brandon Nguyen
+ *
+ * @version October 16, 2016
+ *
+ */
+public class PrintPractice {
+
+    public static void main(String[] args) {
+
+        System.out.println("Jane Doe");
+        System.out.println();
+
+        System.out.println("Hello World!");
+        System.out.println("(And all the people of the world)");
+        System.out.println();
+
+        System.out.println("    *    ");
+        System.out.println("   ***   ");
+        System.out.println("  *****  ");
+        System.out.println(" ******* ");
+        System.out.println("*********");
+        System.out.println("    *    ");
+
+    }
+
+}
