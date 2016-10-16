62d5dd8473d6497ce0b62fddad92ac5068888d2d
Brandon <lrbn@users.noreply.github.com>
2016-10-16 01:31:29 -0400

    Initial commit

---
+++ Part01/src/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * HelloWorld.java
+ *
+ * A test program.
+ *
+ * @author Brandon Nguyen
+ *
+ * @version October 16, 2016
+ *
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
